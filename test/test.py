8fc117e159b2f9ec760e968b8b368e2173569761
amix the lucky stiff <amix@amix.dk>
2010-06-29 19:29:13 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,11 +9,25 @@ s3_uploader = S3Uploader(AWS_KEY,
                          AWS_SECRET,
                          'backups.your_domain.com')
 
-#--- Backup directory ----------------------------------------------
-file_key = generate_file_key('test_dir.%(weekday)s.tar.gz')
+#--- Easy usage ----------------------------------------------
+#Daily
+s3_uploader.compress_and_upload('test_dir/',
+                                'test_dir.%(weekday)s.tgz',
+                                replace_old=True)
 
+#Monthly
+s3_uploader.compress_and_upload('test_dir/',
+                                'test_dir.%(month_name)s.tgz',
+                                replace_old=True)
+
+#Weekly
+s3_uploader.compress_and_upload('test_dir/',
+                                'test_dir.%(week_number)s.tgz',
+                                replace_old=True)
+
+
+#--- Generic usage ----------------------------------------------
+file_key = generate_file_key('test_dir.%(weekday)s.tgz')
 gz_filename = gunzip_dir('test_dir/', file_key)
-
-s3_uploader.upload(file_key, gz_filename, delete_old=True)
-
+s3_uploader.upload(file_key, gz_filename, replace_old=True)
 os.remove(gz_filename)
